1d972f27d96b67b768d130b3b3815c4769a1b482
Noah Zemlin <noah.zemlin@gmail.com>
2020-02-14 14:16:31 -0600

    Project 1 skeleton

---
+++ bb_ws/src/project1_pkg/src/planning_node.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def main():
+    rospy.init_node('planning_node', anonymous=True)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
