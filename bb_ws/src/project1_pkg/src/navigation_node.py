7c3d588845147892e0036211aa860f996c88278e
lae111 <logan.a.elliott-1@ou.edu>
2020-02-14 14:28:45 -0600

    comments!

--- bb_ws/src/project1_pkg/src/navigation_node.py
+++ bb_ws/src/project1_pkg/src/navigation_node.py
@@ -5,9 +5,13 @@ from std_msgs.msg import String
 
 def main():
     rospy.init_node('navigation_node', anonymous=True)
+    # Halt if bumper trigger
+    
+    # Let keyboard input control
+
+    # Do event from planning
 
     rospy.spin()
-
 if __name__ == '__main__':
     try:
         main()
