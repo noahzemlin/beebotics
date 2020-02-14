720a6ea088c6222abf3625b34b572aa9f684ed4f
1jdla <JaclyndlA@ou.edu>
2020-02-14 15:08:14 -0600

    planning outline

--- bb_ws/src/project1_pkg/src/planning_node.py
+++ bb_ws/src/project1_pkg/src/planning_node.py
@@ -2,6 +2,10 @@
 
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import PointCloud2, LaserScan
+from laser_geometry.laser_geometry as lg
+import geometry_msgs
+import random
 
 def main():
     rospy.init_node('planning_node', anonymous=True)
@@ -13,3 +17,23 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
+
+
+def look():
+	rospy.init_node('planning')
+	rospy.Subscriber("looking", String, callback)
+
+# Escape from (roughly) symmetric obstacles within 1ft in front of the robot.
+# Avoid asymmetric obstacles within 1ft in front of the robot.
+# Turn randomly (uniformly sampled within ±15°) after every 1ft of forward movement.
+# Drive forward.
+def escape(self):
+	rospy.Subscriber("/scan", LaserScan, send_command, queue_sizes=1)
+
+def random_turn(self):
+	rand_degree = randint(-15,15)
+	
+	rospy.Publisher("/bb/where2go", Twist, queue_size=1)
+
+main():
+	
